4d585e44cf4c3c9299762c5e961930d8aaf519fb
Fred776 <fredfred776@gmail.com>
2022-04-19 18:28:07 -0400

    Create Register Markup

---
+++ src/components/Register.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+function Register({ onRegister }) {
+  // Input States //
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Event Handlers //
+  const handleEmailChange = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onRegister({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <div className="register page__wrapper">
+      <h3 className="register__title">Sign up</h3>
+      <form className='register__form' onSubmit={handleSubmit} >
+        <input
+          type="email"
+          name="email"
+          className="register__input"
+          id="register__email"
+          value={email || ''}
+          onChange={handleEmailChange}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="text"
+          name="password"
+          className="register__input"
+          id="register__password"
+          value={password || ''}
+          onChange={handlePasswordChange}
+          placeholder="Password"
+          minLength="2"
+          maxLength="30"
+          required
+        />
+        <button className="register__save-button" type="submit">Sign Up</button>
+      </form>
+      <a href=''>Already a member? Log in here!</a>
+    </div>
+  );
+}
+
+export default Register;
