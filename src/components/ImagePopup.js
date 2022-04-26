5c4271489e2ddd1723d15d6d354690b7ea834e36
Fred Jodozi <fredfred776@gmail.com>
2022-04-26 14:31:28 -0400

    rename css blocks for modal images, update css imports and related components

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -4,7 +4,7 @@ function ImagePopup({ card, onClose }) {
   return (
     <Popup type='image-preview' card={card} onClose={onClose}>
       <figure className='modal__figure'>
-        <img className='modal__image_type_image-preview' alt={card ? card.name : ''} src={card ? card.link : ''}/>
+        <img className='modal__image-preview' alt={card ? card.name : ''} src={card ? card.link : ''}/>
         <figcaption className='modal__caption'>{card ? card.name : ''}</figcaption>
         <button className='modal__close-button modal__close-button_type_image' type='button' onClick={onClose} aria-label='close'/>
       </figure>
--- src/components/InfoToolTip.js
+++ src/components/InfoToolTip.js
@@ -8,13 +8,13 @@ function InfoToolTip({ isOpen, onClose, status }) {
       <div className='modal__box modal__box_type_info-tooltip'>
         { status === ('success') ? (
           <>
-            <img className='modal__image_type_info-tooltip' src={authSuccessLogo} alt='Authorization Success' />
-            <p className='modal__message'>Success! You have now been registered!</p>
+            <img className='modal__auth-logo' src={authSuccessLogo} alt='Authorization Success' />
+            <p className='modal__auth-message'>Success! You have now been registered!</p>
           </>
         ) : (
           <>
-            <img className='modal__image_type_info-tooltip' src={authFailureLogo} alt='Authorization Failure' />
-            <p className='modal__message'>Oops, something went wrong! Please try again.</p>
+            <img className='modal__auth-logo' src={authFailureLogo} alt='Authorization Failure' />
+            <p className='modal__auth-message'>Oops, something went wrong! Please try again.</p>
           </>
         ) }
         <button className='modal__close-button modal__close-button_type_info-tooltip' type='button' aria-label='close' onClick={onClose}/>
