ab102a31d8a4960fdf484b5105ff78c871fde3c0
Fred Jodozi <fredfred776@gmail.com>
2022-07-14 07:29:09 -0400

    add .env variables to api and auth url endpoints

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.aroundtheusa.students.nomoreparties.sbs/' : 'http://localhost:3000';
 
 export const getResponseData = (res) => {
   if (!res.ok) {
